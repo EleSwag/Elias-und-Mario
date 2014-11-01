096a6fe119dfcfefabcd6aed188087e47acba551
EleSwag <eliasriederalt@Eliass-MacBook-Pro.local>
2014-11-01 16:13:03 +0100

    Connection Matrix
    Wrote the connections in a matrix (see previous commit)
    rows and lines go from zürich to arthgoldau (analog population vector)

--- code/A (first) Gravity Model/SMSSM.m
+++ code/A (first) Gravity Model/SMSSM.m
@@ -37,8 +37,11 @@ Flow
 %order={Zürich;Genf;Basel;Lausanne;Bern;Winterthur;Luzern;St.
 %Gallen;Lugano;Biel;node Olten;node Arthgoldau(http://de.wikipedia.org/wiki/Eisenbahnknoten#Schweiz)}
 
-populationvector={372857;187470;163216;127821;124381;101308;77491;72959;54667;51203;1770;11062};
+populationvector=[372857;187470;163216;127821;124381;101308;77491;72959;54667;51203;1770;11062];
 
+%connection Matrix
+
+timeconnection=[0 x 71 x 65 19 46 x x x 31 38;x 0 x 33 x x x x x x x x;71 x 0 x x x x x x 67 24 x;x 33 x 0 66 x x x 58 x x x;65 x x 66 0 x 87 x x 25 35 x;19 x x x x 0 x 38 x x x x;50 x x x 87 x 0 x x x 35 27;x x x x x 38 x 0 x x x 101;x x x 58 x x x x 0 x x 117;x x 67 x 25 x x x x 0 37 x;31 x 24 x 35 x 35 x x 37 0 x;38 x x x x x 27 101 117 x x 0];
 
 
 
