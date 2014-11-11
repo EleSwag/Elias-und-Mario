9a3f76bd7a2919ff55f5fe51fba0a7b1aebfe98a
mlv19 <vmario@student.ethz.ch>
2014-11-11 19:50:14 +0100

    distance gravity
    wrote a script that makes a gravity model based network with the
    coordiantes

---
+++ code/A (first) Gravity Model/gravitymodel_distance.m
@@ -0,0 +1,10 @@
+% Generating Gravity model based on distance
+
+% calls data1
+data1;
+
+% run function
+
+% generats network from coordinates
+networkdist=distancecoordinates(cities{2});
+flow=networkflow(cities{3},networkdist);
