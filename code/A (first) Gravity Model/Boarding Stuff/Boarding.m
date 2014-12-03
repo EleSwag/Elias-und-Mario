3f159eb697af0f2a5b0f2fd528a3e1c7d42dd6c7
mlv19 <vmario@student.ethz.ch>
2014-12-03 22:25:17 +0100

    fixed elias-problem
    There were a lot of unnecessary brackets and also one wasn't right at
    all.

--- code/A (first) Gravity Model/Boarding Stuff/Boarding.m
+++ code/A (first) Gravity Model/Boarding Stuff/Boarding.m
@@ -3,4 +3,6 @@ data1;
 
 networkdist=distancecoordinates(cities{2});
 flow=networkflow(cities{3},networkdist);
-scale=(boarding+deboarding)./((sum((flow.*connectionmatrix))');
+scale=(boarding+deboarding)./sum(flow.*connectionmatrix)';
+
+% some comments would be nice
