a7d92e44c8b6943d99b69b7a990b3e86f4f3871e
EleSwag <eliasriederalt@public-docking-hg-4761.ethz.ch>
2014-12-12 17:02:28 +0100

    work together on doc
    fine tuning

--- code/Gravity Model/Compare to real flow/compare.m
+++ code/Gravity Model/Compare to real flow/compare.m
@@ -16,14 +16,17 @@ values1=(boarding+deboarding)./Flux1;
 Flux2=sum(flowconnect,2);
 values2=(boarding+deboarding)./Flux2;
 
+
 % make graphics
 bar(values1);
+ylim([0,12*10^5]);
 xlabel('Cities');
 ylabel('relativ factor');
 title('Comparison with real data and Network 1','Fontsize',14);
 
 figure
 bar(values2);
+ylim([0,12*10^5]);
 xlabel('Cities');
 ylabel('relativ factor');
 title('Comparison with real data and Network 2','Fontsize',14);
